a23c40dfa1215129e2e3419d09387c8aef49e6a4
Anastasia Marhun <Anastasia_Marhun@epam.com>
2021-03-06 23:40:50 +0300

    add solution 'Recursive Depth'

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,6 +2,25 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
+    let self = this;
+    let cnt = 1;
+    let max_cnt = 1;
 
+    arr.forEach(function (elem) {
+
+      if (Array.isArray(elem)) {
+        cnt = 1 + self.calculateDepth(elem);
+
+
+
+      }
+      max_cnt = (cnt > max_cnt) ? cnt : max_cnt;
+    });
+
+
+
+
+    return max_cnt;
   }
+
 };
